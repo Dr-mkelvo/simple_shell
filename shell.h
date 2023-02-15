562aa3aceedee7e82ac362333863d6d86a37aa28
Dr-mkelvo <kelvincent002@gmail.com>
2023-02-15 11:26:01 -0800

    includes shell.h file

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h> /* for printf*/
+#include <unistd.h> /* for fork, execve*/
+#include <stdlib.h>
+#include <string.h> /* for strtok*/
+#include <stddef.h>
+#include <errno.h> /* for errno and perror */
+#include <sys/types.h> /* for type pid */
+#include <sys/wait.h> /* for wait */
+#include <sys/stat.h> /* for use of stat function */
+#include <signal.h> /* for signal management */
+#include <fcntl.h> /* for open files*/
+
+#endif /* SHELL_H */
