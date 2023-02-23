16692db76818390396ae3c9a9ffa7848bb40a7fe
Yuri <yurijamesmail@gmail.com>
2023-02-23 07:38:41 +0300

    helper p

--- helper_p.c
+++ helper_p.c
@@ -2,7 +2,7 @@
 
 /**
  * _print - writes a array of chars in the standar output
- * @string: pointer to the array of chars
+ * @string: pointer to the array of chars.
  * Return: the number of bytes writed or .
  * On error, -1 is returned, and errno is set appropriately.
  */
