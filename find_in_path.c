b18e64f5b769871849eef43c2bbf8a428635004d
Yuri <yurijamesmail@gmail.com>
2023-02-23 07:34:55 +0300

    find in path

--- find_in_path.c
+++ find_in_path.c
@@ -3,7 +3,7 @@
 int check_file(char *full_path);
 
 /**
- * find_program - find a program in path
+ * find_program - find a program in path.
  * @data: a pointer to the program's data
  * Return: 0 if success, errcode otherwise
  */
